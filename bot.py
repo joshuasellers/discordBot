375f82a31744908784d820db4d0cda5bc8f4ec72
Josh Sellers <joshuasellersd@gmail.com>
2020-02-12 19:39:17 -0800

    final part of tutorial

--- bot.py
+++ bot.py
@@ -13,9 +13,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
     print(
         f'{client.user} is connected to the following guild:\n'
