b7c90f737e5b38ebe433d1d76b7e53dd3c70b60e
Josh Sellers <joshuasellersd@gmail.com>
2020-02-15 11:09:54 -0800

    song search command done

--- bot.py
+++ bot.py
@@ -17,6 +17,16 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='search_song', help='Searches for song on YouTube (put song name in quotes)')
 async def song_search(ctx, song: str):
     print(song)
+    results = YoutubeSearch(song, max_results=10).to_json()
+    json_data = json.loads(results)
+    if len(json_data['videos']) > 0:
+        options = []
+        for v in json_data['videos']:
+            song = v['title'] + "\n\tLink: " + v['link'] + "\n\tID: " + v['id']
+            options.append(song)
+        await ctx.send("Here are your results:\n\n" + '\n'.join(options))
+    else:
+        await ctx.send("No Results")
 
 print("BOT RUNNING")
 bot.run(token)
